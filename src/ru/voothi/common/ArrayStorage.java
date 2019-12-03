f854fac9791a3390af1e0df168cd14f2b09d3b8f
Denis Novikov <voothi@gmail.com>
2019-12-03 15:18:56 +0300

    ArrayStorage - Works only index c = 0 Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException at java.lang.System.arraycopy (Native Method) when you remove the index = 1

---
+++ src/ru/voothi/common/ArrayStorage.java
@@ -0,0 +1,37 @@
+package ru.voothi.common;
+
+import java.util.Arrays;
+
+public class ArrayStorage {
+    static int length;
+    static int[] arr;
+
+    static int[] initArr(int... vars) {
+        return vars;
+    }
+
+    public static void main(String[] args) {
+        run();
+    }
+
+    static void run() {
+        fillVal(initArr(1, 2, 3, 0));
+        printArr(arr);
+        delete(1);
+        printArr(arr);
+    }
+
+    static void delete(int index) {
+        System.arraycopy(arr, index + 1, arr, index, length - 1);
+    }
+
+    static void fillVal(int[] arr) {
+        ArrayStorage.arr = arr;
+        length = arr.length;
+    }
+
+    static void printArr(int[] arr) {
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
