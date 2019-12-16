085b3424faf77a373c8adfa3b68ecc833af63904
Denis Novikov <voothi@gmail.com>
2019-12-16 22:31:19 +0300

    Main - if test

--- src/ru/voothi/common/Main.java
+++ src/ru/voothi/common/Main.java
@@ -20,10 +20,50 @@ public class Main {
 
         present
         int insert = -index - 1;
-        if (index !== -1 || index >= 0) {
+        if (index >= 0) {
+            System.out.println("Resume " + resume.getUuid() + " present");
+        } else if (index == -1 || insert == size) {
+            storage[size] = resume;
+            size++;
+        } else {
+            int length = size - insert;
+            System.arraycopy(storage, insert, storage, insert + 1, length);
+            storage[insert] = resume;
+            size++;
+        }
+
+        int insert = -index - 1;
+        if (index >= 0) {
+            System.out.println("Resume " + resume.getUuid() + " present");
+        } else if (index == -1) {
+            method(resume, size);
+        } else {
+            if (insert == size) {
+                method(resume, insert);
+            } else {
+                int length = size - insert;
+                System.arraycopy(storage, insert, storage, insert + 1, length);
+                method(resume, insert);
+            }
+        }
+
+        void method(Resume resume, int index) {
+            storage[index] = resume;
+            size++;
+        }
+
+
+
+        // Эквивалентно?
+        int insert = -index - 1;
+        if (index >= 0) {
             System.out.println("Resume " + resume.getUuid() + " present");
         } else {
-            if (insert == size || index == -1) {
+            if (index == -1) {
+                storage[size] = resume;
+                size++;
+            }
+            if (insert == size) {
                 storage[size] = resume;
                 size++;
             } else {
@@ -33,4 +73,106 @@ public class Main {
                 size++;
             }
         }
+
+        if (case1) {
+            console.log("outcome1")
+        } else {
+            if (case2) {
+                console.log("outcome2")
+            } else {
+                console.log("outcome3")
+            }
+        }
+
+        if (case1) {
+            console.log("outcome1")
+        } else {
+            if (case2) {
+                console.log("outcome2")
+            } else {
+                if (case2) {
+                    console.log("outcome2")
+                } else {
+                    console.log("outcome3")
+                }
+                console.log("outcome3")
+            }
+        }
+
+
+        if (case1) {
+            outcome1
+        } else {
+            if (case2) {
+                outcome2
+            } else {
+                if (case3) {
+                    outcome3
+                } else {
+                    outcome4
+                }
+            }
+        }
+
+        if (case1) {
+            outcome1
+        } else if (case2) {
+            outcome2
+        } else {
+            if (case3) {
+                outcome3
+            } else {
+                outcome4
+            }
+        }
+
+
+        //AND
+        AND;
+        if (case0) {
+            out0
+        } else if (case11 || case12) {
+            true
+        } else {
+            false
+        }
+
+        AND;
+        if (case0) {
+            out0
+        } else if (case11) {
+            if (case12) {
+                true
+            }
+        } else {
+            false
+        }
+
+        //OR
+        OR;
+        if (case0) {
+            out0
+        } else if (case11 || case12) {
+            true
+        } else {
+            false
+        }
+
+        if (case1) {
+            out1
+        } else {
+            if (case2) {
+                out2
+            } else {
+                if (case3) {
+                    out3
+                } else {
+                    out4
+                }
+            }
+        }
+
     }
+
+
+}
