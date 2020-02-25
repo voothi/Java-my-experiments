78917ba1cee9c8cb7cb7a46eefcf81afc3297be9
Denis Novikov <voothi@gmail.com>
2020-02-25 12:50:46 +0300

    # Heroes Team Array names

--- src/ru/voothi/common/Main.java
+++ src/ru/voothi/common/Main.java
@@ -7,15 +7,20 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 //        int arr[] = {1, 2, 3, 4};
-        int n = 4;
-        int arr[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = i + 1;
-        }
-        System.out.println(Arrays.toString(arr));
-        int a = arr[0];
-        int b = arr[arr.length - 1];
-        int rand = (int) ((Math.random() * (b - a + 1) + a));
+//        int n = 4;
+//        int arr[] = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            arr[i] = i + 1;
+//        }
+        String names[] = new String[4];
+        names[0] = "Алена";
+        names[1] = "Ирина";
+        names[2] = "Вася";
+        names[3] = "Алина";
+
+        int rand = (int) (Math.random()*4);
+
         System.out.println(rand);
+        System.out.println(names[rand]);
     }
 }
