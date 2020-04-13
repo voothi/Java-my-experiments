288507e2760dcef34b60e1817af3ee41d1346dd6
Denis Novikov <voothi@gmail.com>
2020-04-13 17:11:03 +0300

    Stream - http://developerkiselev.blogspot.com/2017/03/java-1.html

--- src/ru/voothi/javadevblog/MainStream.java
+++ src/ru/voothi/javadevblog/MainStream.java
@@ -4,24 +4,15 @@ import java.util.function.Function;
 
 public class MainStream {
 
+    public static void main(String[] args) {
+        System.out.println(compute(value -> invert(value), 5));
+    }
+
     public static Integer compute(Function<Integer, Integer> function, Integer value) {
         return function.apply(value);
     }
 
-    public static void main(String[] args) {
-
-    }
-
-    public static class AwesomeClass {
-        private static Integer invert(Integer value) {
-            return -value;
-        }
-
-        public static Integer invertTheNumber() {
-            Integer toInvert = 5;
-            Function<Integer, Integer> invertFunction = value -> invert(value);
-            return compute(invertFunction, toInvert);
-        }
-
+    private static Integer invert(Integer value) {
+        return -value;
     }
 }
