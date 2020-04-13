32a2fdfb5ec775744db5956f07db9814af3bffef
Denis Novikov <voothi@gmail.com>
2020-04-13 13:27:42 +0300

    Stream

--- src/ru/voothi/javadevblog/MainStream.java
+++ src/ru/voothi/javadevblog/MainStream.java
@@ -5,10 +5,13 @@ import java.util.stream.Stream;
 public class MainStream {
     public static void main(String[] args) {
         Stream.of("dd2", "aa2", "bb1", "bb3", "cc4")
-                .filter(s -> {
-                    System.out.println("Фильтр: " + s);
-                    return true;
+                .map(s -> {
+                    System.out.println("map: " + s);
+                    return s.toUpperCase();
                 })
-                .forEach(s -> System.out.println("Печать с использованием forEach: " + s));
+                .anyMatch(s -> {
+                    System.out.println("anyMatch: " + s);
+                    return s.startsWith("A");
+                });
     }
 }
