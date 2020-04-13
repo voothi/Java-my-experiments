73b4face8a2f0d6e9447a55bed578d92ae36d843
Denis Novikov <voothi@gmail.com>
2020-04-13 13:18:27 +0300

    Stream

--- src/ru/voothi/javadevblog/MainStream.java
+++ src/ru/voothi/javadevblog/MainStream.java
@@ -1,11 +1,13 @@
 package ru.voothi.javadevblog;
 
-import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class MainStream {
     public static void main(String[] args) {
-        IntStream.range(1, 4)
-                .mapToObj(i -> "с" + i)
-                .forEach(System.out::println);
+        Stream.of("dd2", "aa2", "bb1", "bb3", "cc4")
+                .filter(s -> {
+                    System.out.println("Фильтр: " + s);
+                    return true;
+                });
     }
 }
