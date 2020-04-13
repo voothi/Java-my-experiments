e6adb02bee4944585b537bb4ae22591e76ca9a9e
Denis Novikov <voothi@gmail.com>
2020-04-13 17:02:46 +0300

    Stream - http://developerkiselev.blogspot.com/2017/03/java-1.html

--- src/ru/voothi/javadevblog/MainStream.java
+++ src/ru/voothi/javadevblog/MainStream.java
@@ -1,33 +1,27 @@
 package ru.voothi.javadevblog;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringJoiner;
-import java.util.function.BiConsumer;
-import java.util.function.Supplier;
-import java.util.stream.Collector;
+import java.util.function.Function;
 
 public class MainStream {
+
+    public static Integer compute(Function<Integer, Integer> function, Integer value) {
+        return function.apply(value);
+    }
+
     public static void main(String[] args) {
 
-        List<Person> persons =
-                Arrays.asList(
-                        new Person("Andrew", 20),
-                        new Person("Igor", 23),
-                        new Person("Ira", 23),
-                        new Person("Vitia", 12));
+    }
 
-        Collector<Person, StringJoiner, String> personNameCollector =
-                Collector.of(
-                        () -> new StringJoiner(" | "),          // supplier
-                        (j, p) -> j.add(p.name.toUpperCase()),  // accumulator
-                        (j1, j2) -> j1.merge(j2),               // combiner
-                        StringJoiner::toString);                // finisher
+    public static class AwesomeClass {
+        private static Integer invert(Integer value) {
+            return -value;
+        }
 
-        String names = persons
-                .stream()
-                .collect(personNameCollector);
+        public static Integer invertTheNumber() {
+            Integer toInvert = 5;
+            Function<Integer, Integer> invertFunction = value -> invert(value);
+            return compute(invertFunction, toInvert);
+        }
 
-        System.out.println(names);  // ANDREW | IGOR | IRA | VITIA
     }
 }
