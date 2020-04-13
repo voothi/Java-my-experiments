55a59a9d9b6aa164860fe440db987459bcce7bfe
Denis Novikov <voothi@gmail.com>
2020-04-13 10:29:52 +0300

    Stream

--- src/ru/voothi/javadevblog/MainStream.java
+++ src/ru/voothi/javadevblog/MainStream.java
@@ -1,13 +1,11 @@
 package ru.voothi.javadevblog;
 
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
 public class MainStream {
     public static void main(String[] args) {
-        Stream.of("c1", "c2", "c3")
-                .map(s -> s.substring(1))
-                .mapToInt(Integer::parseInt)
-                .max()
-                .ifPresent(System.out::println);
+        IntStream.range(1, 4)
+                .mapToObj(i -> "с" + i)
+                .forEach(System.out::println);
     }
 }
