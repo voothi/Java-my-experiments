c1dd829f17f29b6fd4598fc28a24df114991c2ee
Denis Novikov <voothi@gmail.com>
2020-04-17 20:18:46 +0300

    Collection https://javarush.ru/groups/posts/763-9-glavnihkh-voprosov-o-map-v-java

---
+++ src/ru/voothi/javarush/MainCollection.java
@@ -0,0 +1,17 @@
+package ru.voothi.javarush;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class MainCollection {
+    public static void main(String[] args) {
+//        https://javarush.ru/groups/posts/763-9-glavnihkh-voprosov-o-map-v-java
+        //лист ключей
+        List keyList = new ArrayList(Map.keySet());
+//лист значений
+        List valueList = new ArrayList(Map.valueSet());
+//лист ключ-значения
+        List entryList = new ArrayList(Map.entrySet());
+    }
+}
