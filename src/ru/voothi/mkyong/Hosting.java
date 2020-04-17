b05b4a18cf4e88bf1f119119512df7d50bc0c3f2
Denis Novikov <voothi@gmail.com>
2020-04-17 19:57:46 +0300

    Collection - https://mkyong.com/java8/java-8-convert-list-to-map/

---
+++ src/ru/voothi/mkyong/Hosting.java
@@ -0,0 +1,47 @@
+package ru.voothi.mkyong;
+
+public class Hosting {
+
+    private int Id;
+    private String name;
+    private long websites;
+
+    public Hosting(int id, String name, long websites) {
+        Id = id;
+        this.name = name;
+        this.websites = websites;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getWebsites() {
+        return websites;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWebsites(long websites) {
+        this.websites = websites;
+    }
+
+    @Override
+    public String toString() {
+        return "Hosting{" +
+                "Id=" + Id +
+                ", name='" + name + '\'' +
+                ", websites=" + websites +
+                '}';
+    }
+}
---
+++ src/ru/voothi/mkyong/MainCollection.java
@@ -0,0 +1,7 @@
+package ru.voothi.mkyong;
+
+public class MainCollection {
+    public static void main(String[] args) {
+
+    }
+}
