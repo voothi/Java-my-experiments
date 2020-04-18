c5a5ebfba52fdaeb245e129358ac861109885fde
Denis Novikov <voothi@gmail.com>
2020-04-18 22:12:44 +0300

    HashMap HashSet test

--- src/ru/voothi/common/Main.java
+++ src/ru/voothi/common/Main.java
@@ -1,28 +1,23 @@
 package ru.voothi.common;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List list = new ArrayList(15);
-        Map map = new HashMap<String, String>();
+//        Set set = new HashSet();
+//
+//        int i = 1;
+//
+//        set.add(i);
+//        set.add(i);
+//
+//        System.out.println(set);
 
-        list.add("Алена");
-        list.add("Ирина");
-        list.add("Вася");
-        list.add("Алина");
+        HashMap<String, String> map = new HashMap<>();
 
-        int n = 0;
-        int a = 1;
-        int b = list.size() - 1;
-        int rand = (int) ((Math.random() * (b - a + 1) + a));
-        map.put(list.get(n), list.get(rand));
-        list.remove(n);
-        list.remove(rand - 1);
-        System.out.println(list);
+        map.put("k", "v");
+        map.put("k", "v1");
+
         System.out.println(map);
     }
 }
--- src/ru/voothi/javarush/MainCollection.java
+++ src/ru/voothi/javarush/MainCollection.java
@@ -8,10 +8,10 @@ public class MainCollection {
     public static void main(String[] args) {
 //        https://javarush.ru/groups/posts/763-9-glavnihkh-voprosov-o-map-v-java
         //лист ключей
-        List keyList = new ArrayList(Map.keySet());
-//лист значений
-        List valueList = new ArrayList(Map.valueSet());
-//лист ключ-значения
-        List entryList = new ArrayList(Map.entrySet());
+//        List keyList = new ArrayList(Map.keySet());
+////лист значений
+//        List valueList = new ArrayList(Map.valueSet());
+////лист ключ-значения
+//        List entryList = new ArrayList(Map.entrySet());
     }
 }
