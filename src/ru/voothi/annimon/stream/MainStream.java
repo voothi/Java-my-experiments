525d99e2f32e9ff70535d20f9dd3b9e43d4950d4
Denis Novikov <voothi@gmail.com>
2020-04-27 21:43:47 +0300

    https://annimon.com/article/2778 - concat

--- src/ru/voothi/annimon/stream/MainStream.java
+++ src/ru/voothi/annimon/stream/MainStream.java
@@ -1,17 +1,14 @@
 package ru.voothi.annimon.stream;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class MainStream {
     public static void main(String[] args) {
 
-        Stream.iterate(1, x -> x * 2)
-                .limit(6)
+        Stream.concat(
+                Stream.of(1, 2, 3),
+                Stream.of(4, 5, 6))
                 .forEach(System.out::println);
-
 
     }
 }
