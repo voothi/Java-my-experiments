4ef6b374aaded274fc6a45d3ece7e22d4486eeb2
Denis Novikov <voothi@gmail.com>
2020-04-27 21:39:58 +0300

    https://annimon.com/article/2778 - iterate

--- src/ru/voothi/annimon/stream/MainStream.java
+++ src/ru/voothi/annimon/stream/MainStream.java
@@ -1,30 +1,17 @@
 package ru.voothi.annimon.stream;
 
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class MainStream {
     public static void main(String[] args) {
 
-        final List<Integer> list = Arrays.asList(1, 2, 11, 12);
+        Stream.iterate(1, x -> x * 2)
+                .limit(6)
+                .forEach(System.out::println);
 
-        final List<Integer> collect = list.parallelStream()
-                .filter(x -> x > 10)
-                .map(x -> x * 2)
-                .collect(Collectors.toList());
 
-        System.out.println(collect);
-
-        final int sum = IntStream.range(0, 2)
-                .parallel()
-                .map(x -> x * 10)
-                .sum();
-
-        System.out.println(sum);
     }
 }
