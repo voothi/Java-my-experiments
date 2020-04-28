1ed7407855a418a0bad4b8a67d65894fd48bfe70
Denis Novikov <voothi@gmail.com>
2020-04-28 07:55:08 +0300

    https://annimon.com/article/2778 - flatMap

--- src/ru/voothi/annimon/stream/MainStream.java
+++ src/ru/voothi/annimon/stream/MainStream.java
@@ -1,13 +1,13 @@
 package ru.voothi.annimon.stream;
 
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class MainStream {
     public static void main(String[] args) {
 
-        Stream.concat(
-                Stream.of(1, 2, 3),
-                Stream.of(4, 5, 6))
+        Stream.of(2, 3, 0, 1, 3)
+                .flatMapToInt(x -> IntStream.range(0, x))
                 .forEach(System.out::println);
 
     }
