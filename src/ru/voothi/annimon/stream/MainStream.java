d4814fbe559155787862b7bab0dd05cd09915f0c
Denis Novikov <voothi@gmail.com>
2020-04-28 08:00:00 +0300

    https://annimon.com/article/2778 - skip

--- src/ru/voothi/annimon/stream/MainStream.java
+++ src/ru/voothi/annimon/stream/MainStream.java
@@ -6,9 +6,21 @@ import java.util.stream.Stream;
 public class MainStream {
     public static void main(String[] args) {
 
-        Stream.of(2, 3, 0, 1, 3)
-                .flatMapToInt(x -> IntStream.range(0, x))
+        Stream.of(120, 410, 85, 32, 314, 12)
+                .skip(2)
                 .forEach(System.out::println);
+
+        IntStream.range(0, 10)
+                .limit(5)
+                .skip(3)
+                .forEach(System.out::println);
+
+        IntStream.range(0, 10)
+                .skip(5)
+                .limit(3)
+                .skip(1)
+                .forEach(System.out::println);
+
 
     }
 }
