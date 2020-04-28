dcd535be8a7cd73999b8d247f1cb94560045952a
Denis Novikov <voothi@gmail.com>
2020-04-28 14:45:27 +0300

    https://annimon.com/article/2778 - groupingBy

--- src/ru/voothi/annimon/stream/MainStream.java
+++ src/ru/voothi/annimon/stream/MainStream.java
@@ -1,25 +1,51 @@
 package ru.voothi.annimon.stream;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MainStream {
     public static void main(String[] args) {
 
-        String s1 = Stream.of("a", "b", "c", "d")
-                .collect(Collectors.joining());
-        System.out.println(s1);
-// abcd
+        Map<Integer, List<String>> map1 = Stream.of(
+                "ab", "c", "def", "gh", "ijk", "l", "mnop")
+                .collect(Collectors.groupingBy(String::length));
+        map1.entrySet().forEach(System.out::println);
+// 1=[c, l]
+// 2=[ab, gh]
+// 3=[def, ijk]
+// 4=[mnop]
 
-        String s2 = Stream.of("a", "b", "c", "d")
-                .collect(Collectors.joining("-"));
-        System.out.println(s2);
-// a-b-c-d
+        Map<Integer, String> map2 = Stream.of(
+                "ab", "c", "def", "gh", "ijk", "l", "mnop")
+                .collect(Collectors.groupingBy(
+                        String::length,
+                        Collectors.mapping(
+                                String::toUpperCase,
+                                Collectors.joining())
+                ));
+        map2.entrySet().forEach(System.out::println);
+// 1=CL
+// 2=ABGH
+// 3=DEFIJK
+// 4=MNOP
 
-        String s3 = Stream.of("a", "b", "c", "d")
-                .collect(Collectors.joining(" -> ", "[ ", " ]"));
-        System.out.println(s3);
-// [ a -> b -> c -> d ]
+        Map<Integer, List<String>> map3 = Stream.of(
+                "ab", "c", "def", "gh", "ijk", "l", "mnop")
+                .collect(Collectors.groupingBy(
+                        String::length,
+                        LinkedHashMap::new,
+                        Collectors.mapping(
+                                String::toUpperCase,
+                                Collectors.toList())
+                ));
+        map3.entrySet().forEach(System.out::println);
+// 2=[AB, GH]
+// 1=[C, L]
+// 3=[DEF, IJK]
+// 4=[MNOP]
 
     }
 }
