cfa7cdbedb74c8e29c49f9c29a14ac88fac34266
Denis Novikov <voothi@gmail.com>
2020-04-28 10:40:21 +0300

    https://annimon.com/article/2778 - reduce

--- src/ru/voothi/annimon/stream/MainStream.java
+++ src/ru/voothi/annimon/stream/MainStream.java
@@ -1,33 +1,18 @@
 package ru.voothi.annimon.stream;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class MainStream {
     public static void main(String[] args) {
 
-        List<Integer> list = Stream.of(1, 2, 3)
-                .collect(Collectors.toList());
-// list: [1, 2, 3]
-        System.out.println(list);
+        Optional<Integer> result = Stream.<Integer>empty()
+                .reduce(Integer::sum);
+        System.out.println(result.isPresent());
 
-        String s = Stream.of(1, 2, 3)
-                .map(String::valueOf)
-                .collect(Collectors.joining("-", "<", ">"));
-// s: "<1-2-3>"
-
-        System.out.println(s);
-
-        List<String> list1 = Stream.of("a", "b", "c", "d")
-                .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
-// list: ["a", "b", "c", "d"]
-
-        System.out.println(list1);
-
-        String[] elements = Stream.of("a", "b", "c", "d")
-                .toArray(value -> new String[value]);
-
+        Optional<Integer> sum = Stream.of(1, 2, 3, 4, 5)
+                .reduce(Integer::sum);
+        System.out.println(sum.get());
+// 15
     }
 }
