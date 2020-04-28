9043072d4a7f41f3e2999bad952fba26f0102f04
Denis Novikov <voothi@gmail.com>
2020-04-28 08:54:20 +0300

    https://annimon.com/article/2778 - count

--- src/ru/voothi/annimon/stream/MainStream.java
+++ src/ru/voothi/annimon/stream/MainStream.java
@@ -1,15 +1,16 @@
 package ru.voothi.annimon.stream;
 
 import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class MainStream {
     public static void main(String[] args) {
 
-        DoubleStream.of(0.1, Math.PI)
-                .boxed()
-                .map(Object::getClass)
-                .forEach(System.out::println);
+        long count = IntStream.range(0, 10)
+                .flatMap(x -> IntStream.range(0, x))
+                .count();
+        System.out.println(count);
 
     }
 }
