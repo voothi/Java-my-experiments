f75b9681ef7695bafb66c38f5a268e3f6acbdd7f
Denis Novikov <voothi@gmail.com>
2020-04-28 14:19:37 +0300

    https://annimon.com/article/2778 - collectingAndThen

--- src/ru/voothi/annimon/stream/MainStream.java
+++ src/ru/voothi/annimon/stream/MainStream.java
@@ -1,18 +1,70 @@
 package ru.voothi.annimon.stream;
 
-import java.util.Optional;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MainStream {
     public static void main(String[] args) {
 
-        Optional<Integer> result = Stream.<Integer>empty()
-                .reduce(Integer::sum);
-        System.out.println(result.isPresent());
+        Map<Integer, Integer> map1 = Stream.of(1, 2, 3, 4, 5)
+                .collect(Collectors.toMap(
+                        Function.identity(),
+                        Function.identity()
+                ));
 
-        Optional<Integer> sum = Stream.of(1, 2, 3, 4, 5)
-                .reduce(Integer::sum);
-        System.out.println(sum.get());
-// 15
+        System.out.println(map1);
+// {1=1, 2=2, 3=3, 4=4, 5=5}
+
+        Map<Integer, String> map2 = Stream.of(1, 2, 3)
+                .collect(Collectors.toMap(
+                        Function.identity(),
+                        i -> String.format("%d * 2 = %d", i, i * 2)
+                ));
+
+        System.out.println(map2);
+// {1="1 * 2 = 2", 2="2 * 2 = 4", 3="3 * 2 = 6"}
+
+        Map<Character, String> map3 = Stream.of(50, 54, 55)
+                .collect(Collectors.toMap(
+                        i -> (char) i.intValue(),
+                        i -> String.format("<%d>", i)
+                ));
+
+        System.out.println(map3);
+
+        Map<Integer, String> map4 = Stream.of(50, 55, 69, 20, 19, 52)
+                .collect(Collectors.toMap(
+                        i -> i % 5,
+                        i -> String.format("<%d>", i),
+                        (a, b) -> String.join(", ", a, b)
+                ));
+
+// collectingAndThen
+
+        List<Integer> list = Stream.of(1, 2, 3, 4, 5)
+                .collect(Collectors.collectingAndThen(
+                        Collectors.toList(),
+                        Collections::unmodifiableList));
+        System.out.println(list.getClass());
+// class java.util.Collections$UnmodifiableRandomAccessList
+
+        List<String> list2 = Stream.of("a", "b", "c", "d")
+                .collect(Collectors.collectingAndThen(
+                        Collectors.toMap(Function.identity(), s -> s + s),
+                        map -> map.entrySet().stream()))
+                .map(e -> e.toString())
+                .collect(Collectors.collectingAndThen(
+                        Collectors.toList(),
+                        Collections::unmodifiableList));
+        list2.forEach(System.out::println);
+// a=aa
+// b=bb
+// c=cc
+// d=dd
+
     }
 }
