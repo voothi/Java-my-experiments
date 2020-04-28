536ea24c218e524ec3aad8f0ffbcdfa327632abd
Denis Novikov <voothi@gmail.com>
2020-04-28 08:29:01 +0300

    https://annimon.com/article/2778 - sorted

--- src/ru/voothi/annimon/stream/MainStream.java
+++ src/ru/voothi/annimon/stream/MainStream.java
@@ -1,26 +1,22 @@
 package ru.voothi.annimon.stream;
 
+import java.util.Comparator;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class MainStream {
     public static void main(String[] args) {
 
-        Stream.of(120, 410, 85, 32, 314, 12)
-                .skip(2)
-                .forEach(System.out::println);
-
-        IntStream.range(0, 10)
-                .limit(5)
-                .skip(3)
-                .forEach(System.out::println);
-
-        IntStream.range(0, 10)
-                .skip(5)
+        IntStream.concat(
+                IntStream.range(0, 100000000),
+                IntStream.of(-1, -2))
+                .sorted()
                 .limit(3)
-                .skip(1)
                 .forEach(System.out::println);
 
+        Stream.of(120, 410, 85, 32, 314, 12)
+                .sorted(Comparator.naturalOrder())
+                .forEach(System.out::println);
 
     }
 }
