675f02e37211f56201fb81e8eb9c0ee457775ea1
Denis Novikov <voothi@gmail.com>
2020-04-28 10:02:48 +0300

    https://annimon.com/article/2778 - collect - toArray

--- src/ru/voothi/annimon/stream/MainStream.java
+++ src/ru/voothi/annimon/stream/MainStream.java
@@ -1,14 +1,33 @@
 package ru.voothi.annimon.stream;
 
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MainStream {
     public static void main(String[] args) {
 
-        System.out.println(
-                IntStream.rangeClosed(-3, 6)
-                        .count()
-        );
+        List<Integer> list = Stream.of(1, 2, 3)
+                .collect(Collectors.toList());
+// list: [1, 2, 3]
+        System.out.println(list);
+
+        String s = Stream.of(1, 2, 3)
+                .map(String::valueOf)
+                .collect(Collectors.joining("-", "<", ">"));
+// s: "<1-2-3>"
+
+        System.out.println(s);
+
+        List<String> list1 = Stream.of("a", "b", "c", "d")
+                .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+// list: ["a", "b", "c", "d"]
+
+        System.out.println(list1);
+
+        String[] elements = Stream.of("a", "b", "c", "d")
+                .toArray(value -> new String[value]);
 
     }
 }
