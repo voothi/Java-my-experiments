6c1a0b3a970c0daebbb6abc1e601393156b0502d
Denis Novikov <voothi@gmail.com>
2020-05-03 18:39:37 +0300

    - Maven - Servlet

---
+++ src/main/java/ru/voothi/annimon/stream/MainStream.java
@@ -0,0 +1,17 @@
+package ru.voothi.annimon.stream;
+
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class MainStream {
+    public static void main(String[] args) {
+
+        Stream.of(2, 3, 0, 1, 3)
+                .flatMapToInt(x -> IntStream.range(0, x))
+                .forEach(System.out::println);
+
+    }
+
+}
